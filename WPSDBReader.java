00518fd8d5a979d782840df651c49d4dc531c620
digitaldivvy <digitaldivvy@gmail.com>
2017-09-15 15:35:37 -0400

    config

--- WPSDBReader.java
+++ WPSDBReader.java
@@ -3,6 +3,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class WPSDBReader 
     {
@@ -11,8 +14,23 @@ public class WPSDBReader
             try
             {
                 Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                Properties prop = new Properties();
+                String url=null;
+                String userName=null;
+                String password=null;
+                InputStream input=null;
+                try{
+                    input= new FileInputStream("config.properties");
+                    prop.load(input);
+                    url=prop.getProperty("url")+";databaseName=pidcodb";
+                    userName=prop.getProperty("userName");
+                    password=prop.getProperty("password");
 
-              
+                }catch(Exception e)
+                {
+                    System.err.println(e);
+                }
+                
                 Connection con = DriverManager.getConnection(url, userName, password);
                 Statement s1 = con.createStatement();
                 ResultSet rs = s1.executeQuery("SELECT * from lots");
